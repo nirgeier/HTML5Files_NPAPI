ac3672c8b42ae6982b73c2c62aa37c19e9960857
Nir geier <nir@conduit.com>
2011-12-18 10:10:00 +0200

    NPAPI updates

--- NPAPI/contentScript.js
+++ NPAPI/contentScript.js
@@ -5,3 +5,8 @@ var port = chrome.extension.connect({
 port.postMessage({
     'url': document.querySelector('img').src
 });
+
+
+port.onMessage.addListener(function(message) {
+    console.log(message.message + ' is now saved on your local computer');
+});
